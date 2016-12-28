6278523da54e703cf01cbd6941dbab3617f13dfc
Darren Arcangel <darrenarcangel@gmail.com>
2016-12-28 11:51:15 -0800

    Committing "Variables.js" file to GitHub

---
+++ variables.js
@@ -0,0 +1,13 @@
+var carName = "Porsche";						// String
+var maxSpeed = 12.0;							// Number
+var hasAutomaticTransmission = true;			// Boolean
+var car = {};									// Object
+
+console.log(typeof carName);
+console.log(typeof maxSpeed);
+console.log(typeof hasAutomaticTransmission);
+console.log(typeof car);
+
+var myCurrentCar = null;
+var myBestFavoriteCar;
+console.log(myBestFavoriteCar);					// Implicity assigned 'undefined'
